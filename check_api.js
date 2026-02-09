73ff38c281bfbe6b2aaf5a2c5d00993105c97fff
HyeonWooNa0861 <nahw0861@gmail.com>
2026-02-09 13:31:39 +0900

    바꿈요

--- check_api.js
+++ check_api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 async function checkTotal() {
     try {
-        const response = await axios.get('https://opener-api.ap-northeast-2.elasticbeanstalk.com/wines?page=1&per_page=100');
+        const response = await axios.get('https://opener-api.onrender.com/wines?page=1&per_page=100');
         console.log('Total wines reported by API:', response.data.total);
         console.log('Wines in first page:', response.data.wines.length);
     } catch (error) {
